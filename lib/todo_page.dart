0f08665318495f1d61736972425261a4bc30f11e
shreykiee <shreyash33345@gmail.com>
2024-07-24 19:32:17 +0530

    Starting of a to do app

---
+++ lib/todo_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: const Center(child: Text("TO DO")),
+      ),
+      body: ListView(
+        children: [
+          ToDoList(),
+        ],
+      ),
+    );
+  }
+}
